6cb4e89d79333cc399f167b8b8f70a0e3eeb3e60
Mhirax <olowookeremiracle14@gmail.com>
2025-11-21 14:30:11 +0100

    introduction to arrow function

---
+++ arrow.js
@@ -0,0 +1,10 @@
+//INTRO TO ARROW FUNCTION 
+// it is a new and mordern syntax for writing and defining the function expression..
+
+const square = ( x ) => { 
+     return  x * x ;
+ }
+
+const sum = ( x, y ) => {  
+       return x + y ;
+}
