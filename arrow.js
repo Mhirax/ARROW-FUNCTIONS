a2d97d4aaecf359ae2bd74709b5799390c15bf14
Mhirax <olowookeremiracle14@gmail.com>
2025-11-22 16:06:40 +0100

    declaring a random number between 1-6 using arrow function

--- arrow.js
+++ arrow.js
@@ -12,7 +12,11 @@ const sum = ( x, y ) => {
 
 //we always store our arrow function in variable first
 
-
+//They are not supported in internet explorer which is common for new features 
 
 //Arrow functions are especially useful when passing a function as an argument, such as to the mapfunction.
 
+//Declaring a random number between 1 - 6
+
+// const rollDie = ( ) =>  {
+// return  Math.floor (Math.random ( ) * 6) + 1
